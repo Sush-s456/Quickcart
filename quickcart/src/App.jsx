afbfe700fa93e44c3187493b9ed05d703101c3d2
Sushanth H S <sushanthhs365@gmail.com>
2026-03-13 10:59:43 +0530

    Part 2: Added shopping cart functionality

--- quickcart/src/App.jsx
+++ quickcart/src/App.jsx
@@ -1,12 +1,67 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
+import CartSidebar from "./components/CartSidebar";
+import products from "./data/products";
 import "./styles/App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const addToCart = (product) => {
+    const existingItem = cart.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    if (quantity <= 0) {
+      removeFromCart(id);
+    } else {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, quantity } : item
+        )
+      );
+    }
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const getTotalItems = () => {
+    return cart.reduce((total, item) => total + item.quantity, 0);
+  };
+
   return (
-    <div>
-      <Header />
-      <ProductList />
+    <div className="app">
+      <Header cartItemCount={getTotalItems()} onCartClick={toggleCart} />
+
+      <ProductList products={products} onAddToCart={addToCart} />
+
+      <CartSidebar
+        isOpen={isCartOpen}
+        onClose={toggleCart}
+        cart={cart}
+        onUpdateQuantity={updateQuantity}
+        onRemoveItem={removeFromCart}
+      />
     </div>
   );
 }
